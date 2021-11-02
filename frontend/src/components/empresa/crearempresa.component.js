4ebc5d334ca895973ec679a83b9d576e9687b70d
AuxZarske <43278023+AuxZarske@users.noreply.github.com>
2021-11-02 00:04:16 -0300

    se agregaron los campos faltantes a empresa

--- frontend/src/components/empresa/crearempresa.component.js
+++ frontend/src/components/empresa/crearempresa.component.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { forwardRef, useRef, useImperativeHandle } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
@@ -11,46 +11,126 @@ import EmpresaService from '../../services/empresa.service'
 import Divider from '@material-ui/core/Divider';
 import swal from 'sweetalert';
 
-export default class CrearEmpresa extends Component {
-  constructor(props) {
-    super(props);
-    this.child = React.createRef();
 
-    this.onChangeName = this.onChangeName.bind(this);
-    
-    this.saveEmpresa = this.saveEmpresa.bind(this);
 
-    this.state = {
-      id: null,
-      name: ""
-    };
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+export default function CrearEmpresa() {
+  const childRef = useRef();
+
+  const [state, setState] = React.useState({
+    id: null,
+    name: "",
+    tipo: "",
+    telefono: '',
+    web: '',
+    cuit: '',
+    provincia: "",
+    localidad: '',
+    codigoPostal: '',
+    calle: '',
+  });
+
+
+
+  function onChangeName(e) {
+    setState({
+      ...state,
+      name: e.target.value,
+    });
   }
 
-  onChangeName(e) {
-    this.setState({
-      name: e.target.value
+  function onChangeCuit(e) {
+    setState({
+      ...state,
+      cuit: e.target.value
+    });
+  }
+  function onChangeTelefono(e) {
+    setState({
+      ...state,
+      telefono: e.target.value
+    });
+  }
+  function onChangeWeb(e) {
+    setState({
+      ...state,
+      web: e.target.value
+    });
+  }
+  function onChangeTipo(e) {
+    setState({
+      ...state,
+      tipo: e.target.value
+    });
+  }
+  function onChangeProvincia(e) {
+    setState({
+      ...state,
+      provincia: e.target.value
+    });
+  }
+
+  function onChangeLocalidad(e) {
+    setState({
+      ...state,
+      localidad: e.target.value
+    });
+  }
+  function onChangeCalle(e) {
+    setState({
+      ...state,
+      calle: e.target.value
+    });
+  }
+  function onChangeCodigoPostal(e) {
+    setState({
+      ...state,
+      codigoPostal: e.target.value
     });
   }
 
 
 
-  saveEmpresa() {
+  function saveEmpresa() {
     var data = {
-      name: this.state.name,
+      name: state.name,
+      tipo: state.tipo,
+      cuit: state.cuit,
+      direccion: state.calle,
+      telefono: state.telefono,
+      web: state.web,
     };
-
+    console.log(data);
     EmpresaService.create(data)
       .then(response => {
-        this.setState({
+        setState({
           id: null,
           name: '',
+          tipo: "",
+          telefono: '',
+          web: '',
+          cuit: '',
+          provincia: "",
+          localidad: '',
+          codigoPostal: '',
+          calle: '',
         });
         console.log(response.data);
-        this.child.current.refreshList();
+        childRef.current.refreshList();
         // borrar text
-        document.getElementById("empresanombre").value='';
-          //msj cargado exitosament
-          swal("Correcto!", "Se agrego con exito a la tabla!", "success");
+        document.getElementById("empresanombre").value = '';
+        //msj cargado exitosament
+        swal("Correcto!", "Se agrego con exito a la tabla!", "success");
 
       })
       .catch(e => {
@@ -59,51 +139,100 @@ export default class CrearEmpresa extends Component {
       });
   }
 
-  render (){
 
-    return (
+  const classes = useStyles();
+  return (
     <Grid
-    container
-    xs={12}
-    direction="column"
-    alignItems="center"
+      container
+      xs={12}
+      direction="column"
+      alignItems="center"
     >
-      
+
       <Grid
-    
-    xs={7}
-    direction="column"
-    alignItems="flex-start"
-    >
-      
 
-      
+        xs={7}
+        direction="column"
+        alignItems="flex-start"
+      >
 
-      <Grid item>
-        
-        <Typography variant="h5" >
-          Crear empresa
-        </Typography>
-        <TextField id="empresanombre" label="Nombre de empresa" color="secondary" onChange={this.onChangeName} />
-        <Button variant="contained" color="primary" onClick={this.saveEmpresa}>
-          Crear
+
+
+
+        <Grid item>
+
+          <Typography variant="h5" >
+            Crear empresa
+          </Typography>
+          <br></br>
+          <Grid
+          container
+          alignItems="center"
+          justifyContent="center">
+            <TextField style={{ margin: 15 }} id="empresanombre" value={state.name} label="Nombre de empresa" color="secondary" onChange={onChangeName} />
+            <TextField style={{ margin: 15 }} id="cuit" label="CUIT" value={state.cuit} color="secondary" onChange={onChangeCuit} />
+            <TextField style={{ margin: 15 }} id="telefono" label="Telefono" value={state.telefono} color="secondary" onChange={onChangeTelefono} />
+            <TextField style={{ margin: 15 }} id="web" label="Web" value={state.web} color="secondary" onChange={onChangeWeb} />
+            <TextField style={{ margin: 15 }} id="tipo" label="tipo" value={state.tipo} color="secondary" onChange={onChangeTipo} />
+
+
+            <input
+        accept="image/*"
+        className={classes.input}
+        id="contained-button-file"
+        multiple
+        type="file"
+      />
+      <label htmlFor="contained-button-file">
+        <Button variant="contained" color="primary" component="span" style={{marginTop:20}}>
+          Logo / imagen
         </Button>
-        
+      </label>
+
+
+          </Grid>
+
+        </Grid>
+
+        <br></br>
+        <Divider></Divider>
+        <br></br>
+        <Typography variant="h6" >
+          Dirección:
+        </Typography>
+        <Grid
+          container
+          alignItems="center"
+          justifyContent="center"
+        >
+
+
+          <TextField style={{ margin: 5 }} id="provincia"  label="Provincia" color="secondary" onChange={onChangeProvincia} />
+          <TextField style={{ margin: 5 }} id="localidad"  label="Localidad" color="secondary" onChange={onChangeLocalidad} />
+          <TextField style={{ margin: 5 }} id="codigoPostal"  label="Codigo postal" color="secondary" onChange={onChangeCodigoPostal} />
+          <TextField style={{ margin: 5 }} id="calle" value={state.calle} label="Calle y numero" color="secondary" onChange={onChangeCalle} />
+
+
+        </Grid>
+        <br></br>
+        <Divider></Divider>
+        <center>
+          <Button variant="contained" color="primary" onClick={saveEmpresa} style={{ margin: 8 }}>
+            Crear
+          </Button>
+        </center>
+        <br></br>
+        <Grid item>
+          <Listaempresas ref={childRef} />
+        </Grid>
       </Grid>
-      <br></br>
-      <Divider></Divider>
-      <br></br>
-      <Grid item>
-        <Listaempresas ref={this.child}/>
-      </Grid>
-    </Grid>
 
     </Grid>
-    );
+  );
 
-  }
+
 
 
 
 }
- 
+
