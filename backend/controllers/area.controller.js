41c30c798102db520c71dc2ddd02fe72255667ae
Santiago Villar <santiago.villar@unam.edu.ar>
2022-08-05 10:44:36 -0300

    Dockerizando

--- backend/controllers/area.controller.js
+++ backend/controllers/area.controller.js
@@ -2,122 +2,116 @@ const db = require("../models");
 const Area = db.areas;
 const Empresa = db.empresas;
 
-
 exports.create = (req, res) => {
   //Se valida la request
-  if(!req.body.idEmpresa || !req.body.name){
-      res.status(400).send({ message: 'Falta algun campo xd'});
-      return;
+  if (!req.body.idEmpresa || !req.body.name) {
+    res.status(400).send({ message: "Falta algun campo xd" });
+    return;
   }
-  console.log("ID EMPRESA: ", req.body.idEmpresa)
+  console.log("ID EMPRESA: ", req.body.idEmpresa);
   //Se crea el area con lo recibido
   const area = new Area({
-      name: req.body.name,
-      departamentos: [],
+    name: req.body.name,
+    departamentos: [],
   });
 
   //Obtenemos la empresa del area a crear
-  
 
   //Se guarda el Area en la db
   area
-  .save(area)
-  .then(data => {
-        console.log('data: ', data.id);
-      
-        //Asociamos el area a la empresa
-        return Empresa.findByIdAndUpdate(req.body.idEmpresa, {
-            $push: {
-                areas: data.id
-            }            
-        }, {new: true, useFindAndModify: false})
-        
-      
-  })
-    .then(data => {
-        res.send({message: 'Se creo el area correctamente'});
+    .save(area)
+    .then((data) => {
+      console.log("data: ", data.id);
+
+      //Asociamos el area a la empresa
+      return Empresa.findByIdAndUpdate(
+        req.body.idEmpresa,
+        {
+          $push: {
+            areas: data.id,
+          },
+        },
+        { new: true, useFindAndModify: false }
+      );
     })
-    
-  .catch(err => {
+    .then((data) => {
+      res.send({ message: "Se creo el area correctamente" });
+    })
+
+    .catch((err) => {
       res.status(500).send({
-          message: err.message
+        message: err.message,
       });
-  });
-
-};
-
-
-exports.findAll = (req, res) => {
-    Area.find({})
-    .then(data => {
-        res.send(data);
-    })
-    .catch(err => {
-        res.status(500).send({
-            message: err.message
-        });
     });
 };
 
+exports.findAll = (req, res) => {
+  Area.find({})
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message,
+      });
+    });
+};
 
 exports.findOne = (req, res) => {
-    const id = req.params.id;
+  const id = req.params.id;
 
-    Area.findById(id)
-    .then(data => {
-        if (!data)
-            res.status(404).send({ message: "No se encontro una Area con ese Id."});
-        else res.send(data);
-    })
-    .catch(err => {
+  Area.findById(id)
+    .then((data) => {
+      if (!data)
         res
-        .status(500)
-        .send({ message: err.message})
+          .status(404)
+          .send({ message: "No se encontro una Area con ese Id." });
+      else res.send(data);
     })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
 };
-
 
 exports.update = (req, res) => {
-    if (!req.body){
-        return res.status(400).send({
-            message: "Los campos para actualizar no pueden estar vacios"
-        });
-    }
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Los campos para actualizar no pueden estar vacios",
+    });
+  }
 
-    const id = req.params.id;
+  const id = req.params.id;
 
-    Area.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-    .then(data => {
-        if (!data)
-            res.status(404).send({ message: "No se encontro una Area con ese Id."});
-        else res.send({ message: "Area Actualizada", id: data.id});
-    })
-    .catch(err => {
+  Area.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data)
         res
-        .status(500)
-        .send({ message: err.message})
+          .status(404)
+          .send({ message: "No se encontro una Area con ese Id." });
+      else res.send({ message: "Area Actualizada", id: data.id });
     })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
 };
-
 
 exports.delete = (req, res) => {
   const id = req.params.id;
   Area.findByIdAndRemove(id)
-  .then(data => {
-    if (!data) {
+    .then((data) => {
+      if (!data) {
         res.status(404).send({
-            message: "No se encontro la Area con ese id."
+          message: "No se encontro la Area con ese id.",
         });
-    } else {
+      } else {
         res.send({
-            message: "La Area se borró correctamente."
+          message: "La Area se borró correctamente.",
         });
-    }
-  })
-  .catch(err => {
-    res.status(500).send({
-      message: "No se pudo eliminar la Area " + err.message
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "No se pudo eliminar la Area " + err.message,
+      });
     });
-  });
 };
-
--- backend/controllers/convenio.controller.js
+++ backend/controllers/convenio.controller.js
@@ -5,11 +5,10 @@ const CategoriaConv = db.categorias_conv;
 const OpcionesBasicas = db.convenios;
 const SumasDescuentos = db.sumas_descuentos;
 
-
 exports.create = (req, res) => {
     //Se valida la request
     if (!req.body) {
-        res.status(400).send({ message: 'Falta algun campo xd' });
+        res.status(400).send({ message: "Falta algun campo xd" });
         return;
     }
 
@@ -19,106 +18,105 @@ exports.create = (req, res) => {
         vigente_desde: req.body.vigente_desde,
         categorias: [],
         sumas_remunerativas: [],
-
     });
-
-
 
     //Se guarda el convenio en la db
     convenio
         .save(convenio)
-        .then(data => {
-            res.send({ message: 'Se creo el convenio correctamente', data: data });
+        .then((data) => {
+            res.send({
+                message: "Se creo el convenio correctamente",
+                data: data,
+            });
         })
-        .catch(err => {
+        .catch((err) => {
             res.status(500).send({
-                message: err.message
+                message: err.message,
             });
         });
-
 };
-
 
 exports.findAll = (req, res) => {
-    Convenio.find().populate('sumas_descuentos').populate({
-        path: 'categorias',
-        populate: {
-            path: 'subcategorias'
-        }
+    Convenio.find()
+        .populate("sumas_descuentos")
+        .populate({
+            path: "categorias",
+            populate: {
+                path: "subcategorias",
+            },
         })
-        .then(data => {
+        .then((data) => {
             res.send(data);
         })
-        .catch(err => {
+        .catch((err) => {
             res.status(500).send({
-                message: err.message
+                message: err.message,
             });
         });
 };
-
 
 exports.findOne = (req, res) => {
     const id = req.params.id;
 
-    Convenio.findById(id).populate('sumas_descuentos').populate({
-        path: 'categorias',
-        populate: {
-            path: 'subcategorias'
-        }
+    Convenio.findById(id)
+        .populate("sumas_descuentos")
+        .populate({
+            path: "categorias",
+            populate: {
+                path: "subcategorias",
+            },
         })
-        .then(data => {
+        .then((data) => {
             if (!data)
-                res.status(404).send({ message: "No se encontro una convenio con ese Id." });
+                res.status(404).send({
+                    message: "No se encontro una convenio con ese Id.",
+                });
             else res.send(data);
         })
-        .catch(err => {
-            res
-                .status(500)
-                .send({ message: err.message })
-        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message });
+        });
 };
-
 
 exports.update = (req, res) => {
     if (!req.body) {
         return res.status(400).send({
-            message: "Los campos para actualizar no pueden estar vacios"
+            message: "Los campos para actualizar no pueden estar vacios",
         });
     }
 
     const id = req.params.id;
 
     Convenio.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
-        .then(data => {
+        .then((data) => {
             if (!data)
-                res.status(404).send({ message: "No se encontro un Convenio con ese Id." });
+                res.status(404).send({
+                    message: "No se encontro un Convenio con ese Id.",
+                });
             else res.send({ message: "Convenio Actualizado", id: data.id });
         })
-        .catch(err => {
-            res
-                .status(500)
-                .send({ message: err.message })
-        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message });
+        });
 };
-
 
 exports.delete = (req, res) => {
     const id = req.params.id;
     Convenio.findByIdAndRemove(id)
-        .then(data => {
+        .then((data) => {
             if (!data) {
                 res.status(404).send({
-                    message: "No se encontro El Convenio con ese id."
+                    message: "No se encontro El Convenio con ese id.",
                 });
             } else {
                 res.send({
-                    message: "El Convenio se borró correctamente."
+                    message: "El Convenio se borró correctamente.",
                 });
             }
         })
-        .catch(err => {
+        .catch((err) => {
             res.status(500).send({
-                message: "No se pudo eliminar El Convenio " + err.message
+                message: "No se pudo eliminar El Convenio " + err.message,
             });
         });
 };
@@ -127,13 +125,15 @@ exports.agregarCategoria = (req, res) => {
     const id = req.params.id;
 
     Convenio.findById(id)
-        .then(data => {
+        .then((data) => {
             if (!data)
-                res.status(404).send({ message: "No se encontro una convenio con ese Id." });
+                res.status(404).send({
+                    message: "No se encontro una convenio con ese Id.",
+                });
             else {
                 //Se valida la request
                 if (!req.body) {
-                    res.status(400).send({ message: 'Falta algun campo xd' });
+                    res.status(400).send({ message: "Falta algun campo xd" });
                     return;
                 }
 
@@ -146,160 +146,178 @@ exports.agregarCategoria = (req, res) => {
                 return categoria_conv.save(categoria_conv);
             }
         })
-        .then(data => {
+        .then((data) => {
             cat_agregada = data;
-            return Convenio.findByIdAndUpdate(id, {
-                $push: {
-                    categorias: data.id
-                }
-            }, { new: true, useFindAndModify: false })
+            return Convenio.findByIdAndUpdate(
+                id,
+                {
+                    $push: {
+                        categorias: data.id,
+                    },
+                },
+                { new: true, useFindAndModify: false }
+            );
         })
-        .then(data => {
-            res.send({ message: "Se agrego la categoria al convenio correctamente", data: cat_agregada });
+        .then((data) => {
+            res.send({
+                message: "Se agrego la categoria al convenio correctamente",
+                data: cat_agregada,
+            });
         })
-        .catch(err => {
-            res
-                .status(500)
-                .send({ message: err.message })
-        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message });
+        });
 };
 
 exports.quitarCategoria = (req, res) => {
     const idConv = req.params.id;
     const idCat = req.body.idCat;
-    Convenio.findByIdAndUpdate(idConv, {
+    Convenio.findByIdAndUpdate(
+        idConv,
+        {
             $pull: {
-                categorias: idCat
-            }
-        }, { new: true, useFindAndModify: false })
-        .then(data => {
-            CategoriaConv.findByIdAndRemove(idCat)
-                .then(data => {
-                    if (!data) {
-                        res.status(404).send({
-                            message: "No se encontro La Categoria con ese id."
-                        });
-                    } else {
-                        res.send({
-                            message: "La Categoria se borró correctamente."
-                        });
-                    }
-                })
+                categorias: idCat,
+            },
+        },
+        { new: true, useFindAndModify: false }
+    )
+        .then((data) => {
+            CategoriaConv.findByIdAndRemove(idCat).then((data) => {
+                if (!data) {
+                    res.status(404).send({
+                        message: "No se encontro La Categoria con ese id.",
+                    });
+                } else {
+                    res.send({
+                        message: "La Categoria se borró correctamente.",
+                    });
+                }
+            });
         })
-        .catch(err => {
-            res
-                .status(500)
-                .send({ message: err.message })
-        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message });
+        });
 };
 
 exports.agregarSubCategoria = (req, res) => {
     const id = req.params.id;
     const idCat = req.body.idCat;
-    if (!idCat)
-        res.status(400).send({ message: "Falta el campo idCat" })
+    if (!idCat) res.status(400).send({ message: "Falta el campo idCat" });
 
     Convenio.findById(id)
-        .then(data => {
+        .then((data) => {
             if (!data)
-                res.status(404).send({ message: "No se encontro una convenio con ese Id." });
+                res.status(404).send({
+                    message: "No se encontro una convenio con ese Id.",
+                });
             else {
                 //Se valida la request
                 if (!req.body) {
-                    res.status(400).send({ message: 'Falta algun campo xd' });
+                    res.status(400).send({ message: "Falta algun campo xd" });
                     return;
                 }
 
                 //Se crea la categoria del convenio con lo recibido
                 const subcategoria_conv = new SubCategoriaConv({
                     name: req.body.name,
-                    basico: req.body.basico
+                    basico: req.body.basico,
                 });
 
                 return subcategoria_conv.save(subcategoria_conv);
             }
         })
-        .then(data => {
-            return CategoriaConv.findByIdAndUpdate(idCat, {
-                $push: {
-                    subcategorias: data.id
-                }
-            }, { new: true, useFindAndModify: false })
+        .then((data) => {
+            return CategoriaConv.findByIdAndUpdate(
+                idCat,
+                {
+                    $push: {
+                        subcategorias: data.id,
+                    },
+                },
+                { new: true, useFindAndModify: false }
+            );
         })
-        .then(data => {
-            res.send({ message: "Se agrego la subcategoria al convenio correctamente", data: data });
+        .then((data) => {
+            res.send({
+                message: "Se agrego la subcategoria al convenio correctamente",
+                data: data,
+            });
         })
-        .catch(err => {
-            res
-                .status(500)
-                .send({ message: err.message })
-        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message });
+        });
 };
 
 //actualiza monto basico de una sub categoria
 exports.updateSubCategoria = (req, res) => {
     if (!req.body) {
         return res.status(400).send({
-            message: "Los campos para actualizar no pueden estar vacios"
+            message: "Los campos para actualizar no pueden estar vacios",
         });
     }
-//
+    //
     const idCat = req.params.id;
 
-    SubCategoriaConv.findByIdAndUpdate(idCat, req.body, { useFindAndModify: false })
-        .then(data => {
+    SubCategoriaConv.findByIdAndUpdate(idCat, req.body, {
+        useFindAndModify: false,
+    })
+        .then((data) => {
             if (!data)
-                res.status(404).send({ message: "No se encontro una sub categoria con ese Id." });
-            else res.send({ message: "Sub categoria Actualizada", id: data.id });
+                res.status(404).send({
+                    message: "No se encontro una sub categoria con ese Id.",
+                });
+            else
+                res.send({ message: "Sub categoria Actualizada", id: data.id });
         })
-        .catch(err => {
-            res
-                .status(500)
-                .send({ message: err.message })
-        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message });
+        });
 };
 
 exports.quitarSubCategoria = (req, res) => {
     const idCat = req.body.idCat;
     const idSubCat = req.body.idSubCat;
-    CategoriaConv.findByIdAndUpdate(idCat, {
+    CategoriaConv.findByIdAndUpdate(
+        idCat,
+        {
             $pull: {
-                subcategorias: idSubCat
-            }
-        }, { new: true, useFindAndModify: false })
-        .then(data => {
-            SubCategoriaConv.findByIdAndRemove(idSubCat)
-                .then(data => {
-                    if (!data) {
-                        res.status(404).send({
-                            message: "No se encontro La SubCategoria con ese id."
-                        });
-                    } else {
-                        res.send({
-                            message: "La SubCategoria se borró correctamente."
-                        });
-                    }
-                })
+                subcategorias: idSubCat,
+            },
+        },
+        { new: true, useFindAndModify: false }
+    )
+        .then((data) => {
+            SubCategoriaConv.findByIdAndRemove(idSubCat).then((data) => {
+                if (!data) {
+                    res.status(404).send({
+                        message: "No se encontro La SubCategoria con ese id.",
+                    });
+                } else {
+                    res.send({
+                        message: "La SubCategoria se borró correctamente.",
+                    });
+                }
+            });
         })
-        .catch(err => {
-            res
-                .status(500)
-                .send({ message: err.message })
-        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message });
+        });
 };
 
 exports.agregarSumaDescuento = (req, res) => {
     const id = req.params.id;
 
     Convenio.findById(id)
-        .then(data => {
+        .then((data) => {
             if (!data)
-                res.status(404).send({ message: "No se encontro un convenio con ese Id." });
+                res.status(404).send({
+                    message: "No se encontro un convenio con ese Id.",
+                });
             else {
                 //Se valida la request
                 convenio = data;
                 if (!req.body) {
-                    res.status(400).send({ message: 'Falta algun campo xd' });
+                    res.status(400).send({ message: "Falta algun campo xd" });
                     return;
                 }
 
@@ -311,53 +329,55 @@ exports.agregarSumaDescuento = (req, res) => {
                     cantidad: req.body.cantidad,
                     tipo: req.body.tipo,
                     sobre: req.body.sobre,
-
                 });
 
                 return suma_descuento.save(suma_descuento);
-                
             }
         })
-        .then(data => {
+        .then((data) => {
             sumadesc_creada = data;
             convenio.sumas_descuentos.push(sumadesc_creada);
             return convenio.save();
-        })        
-        .then(data => {
-            res.send({ message: "Se agrego la suma o descuento al convenio correctamente", data: sumadesc_creada });
         })
-        .catch(err => {
-            res
-                .status(500)
-                .send({ message: err.message })
+        .then((data) => {
+            res.send({
+                message:
+                    "Se agrego la suma o descuento al convenio correctamente",
+                data: sumadesc_creada,
+            });
         })
+        .catch((err) => {
+            res.status(500).send({ message: err.message });
+        });
 };
 
 exports.quitarSumaDescuento = (req, res) => {
     const id = req.params.id;
     const idSum = req.body.idSum;
-    Convenio.findByIdAndUpdate(id, {
+    Convenio.findByIdAndUpdate(
+        id,
+        {
             $pull: {
-                sumas_descuentos: idSum
-            }
-        }, { new: true, useFindAndModify: false })
-        .then(data => {
-            SumasDescuentos.findByIdAndRemove(idSum)
-                .then(data => {
-                    if (!data) {
-                        res.status(404).send({
-                            message: "No se encontro La Suma o Descuento con ese id."
-                        });
-                    } else {
-                        res.send({
-                            message: "La Suma o Descuento se borró correctamente."
-                        });
-                    }
-                })
+                sumas_descuentos: idSum,
+            },
+        },
+        { new: true, useFindAndModify: false }
+    )
+        .then((data) => {
+            SumasDescuentos.findByIdAndRemove(idSum).then((data) => {
+                if (!data) {
+                    res.status(404).send({
+                        message:
+                            "No se encontro La Suma o Descuento con ese id.",
+                    });
+                } else {
+                    res.send({
+                        message: "La Suma o Descuento se borró correctamente.",
+                    });
+                }
+            });
         })
-        .catch(err => {
-            res
-                .status(500)
-                .send({ message: err.message })
-        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message });
+        });
 };
